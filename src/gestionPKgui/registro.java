876ba72ad70d0f05595cb9a73160728e5929d943
Joel817 <licjoelgutierrez77@outlook.com>
2023-12-10 14:37:39 -0500

    Quinto Commit

--- src/gestionPKgui/registro.java
+++ src/gestionPKgui/registro.java
@@ -11,7 +11,7 @@ import javax.swing.JOptionPane;                                                 
  *
  * @author licjo
  *
- * Joel Gutiérrez
+ * Joel Gutiérrez D.
  * */
 public class registro extends javax.swing.JFrame {                              /** Creación de la clase registro que hereda del JFrame  **/
 
